b18d9e0a15fc98e5f986a7c3557b04006c754709
Gregory Stotts <mr.stotts@gmail.com>
2023-11-16 14:27:56 -0600

    Logging

--- functions/report_to_smartsheet/ReportToSmartsheet.py
+++ functions/report_to_smartsheet/ReportToSmartsheet.py
@@ -1,35 +1,53 @@
 #!/bin/python3
 
 import boto3
-import json
 import urllib.parse
+import logging
 
 region = "us-east-2"
 s3 = boto3.client('s3', region_name=region)
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+logger.setLevel(logging.INFO)
 
-def lambda_handler(event, context):
-
+def get_file_from_s3(event):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
 
     try:
-        print(f'[+] Retrieving Object {key} from {bucket}')
+        logger.info(f'[+] Retrieving Object {key} from {bucket}')
         response = s3.get_object(Bucket=bucket, Key=key)
         if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-            print(f'[-] Error: Invalid Status Code Retrieving Object {key} from {bucket}')
+            logger.error(f'[-] Error: Invalid Status Code Retrieving Object {key} from {bucket}')
             return {
                 'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
                 'error': 'Invalid Status Code Retrieving Object' + key + ' from '+ bucket
             }
         
     except Exception as e:
-        print(f'[-] Error Retreiving Object {key} from {bucket}')
-        print(e)
+        logger.error(f'[-] Error Retreiving Object {key} from {bucket}')
+        logger.error(e)
         raise e
+    logger.info(f'[+] Successfully Retrieved {key} from {bucket}')
 
-    data = response['Body'].read().decode('utf-8')
-    
+    try:
+        data = response['Body'].read().decode('utf-8')
+    except Exception as e:
+        logger.error(f'[-] Error Reading and Decoding File {key}')
+        logger.error(e)
+        raise e
+    logger.info(f'[+] Successfully read data from {key}')
+    return data
+
+def upload_to_smartsheet(data):
+    pass
+
+def lambda_handler(event, context):
+    logger.info(f'[+] Lambda Invocation Starting')
+    data = get_file_from_s3(event)
+    upload_to_smartsheet(data)
+
     return {
-        'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
-        'body': data
+        'statusCode': 200,
+        'body': '[+] Lambda Invocation Complete'
     }
