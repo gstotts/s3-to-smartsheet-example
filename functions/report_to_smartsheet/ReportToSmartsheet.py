82470c218f427aa4892f21cff8e2f37cda1ad182
Gregory Stotts <mr.stotts@gmail.com>
2024-02-10 13:50:53 -0600

    MOdifications

--- functions/report_to_smartsheet/ReportToSmartsheet.py
+++ functions/report_to_smartsheet/ReportToSmartsheet.py
@@ -6,7 +6,9 @@ import logging
 import smartsheet
 import os
 
+FOLDER_ID = "678637063169924"
 region = "us-east-2"
+
 s3 = boto3.client('s3', region_name=region)
 logger = logging.getLogger()
 logger.setLevel(logging.ERROR)
@@ -40,44 +42,68 @@ def get_file_from_s3(event):
         raise e
     logger.info(f'[+] Successfully read data from {key}')
 
-    with open('/tmp/data.csv', 'w') as file:
-        file.write(data)
+    # with open('/tmp/data.csv', 'w') as file:
+    #     file.write(data)
 
-    return '/tmp/data.csv', key.split(".")[0]
+    return data, key.split(".")[0]
 
-def upload_to_smartsheet(temp_file, sheet_name):
-    try:
-        smart = smartsheet.Smartsheet()
-        smart.errors_as_exceptions(True)
-    except ValueError as e:
-        logger.error(f'[-] Error: SMARTSHEET_ACCESS_TOKEN must be set')
-        raise e
+# def upload_to_smartsheet(temp_file, sheet_name):
+#     try:
+#         smart = smartsheet.Smartsheet()
+#         smart.errors_as_exceptions(True)
+#     except ValueError as e:
+#         logger.error(f'[-] Error: SMARTSHEET_ACCESS_TOKEN must be set')
+#         raise e
     
-    sheet = smart.Sheets.get_sheet_by_name(sheet_name)
-    if not sheet:
-        logger.info(f'[+] Creating Sheet: {sheet_name}')
-        sheet = smart.Folders.import_csv_sheet(
-            678637063169924, # folder ID
-            temp_file,
-            sheet_name,
-            header_row_index=0,
-            primary_column_index=0,
-        )
-        sheet = smart.Sheets.get_sheet(sheet.data.id)
-        logger.info(f'[+] Sheet {sheet_name} Created')
-        return
+#     sheet = smart.Sheets.get_sheet_by_name(sheet_name)
+#     if not sheet:
+#         logger.info(f'[+] Creating Sheet: {sheet_name}')
+#         sheet = smart.Folders.import_csv_sheet(
+#             FOLDER_ID, # folder ID
+#             temp_file,
+#             sheet_name,
+#             header_row_index=0,
+#             primary_column_index=0,
+#         )
+#         sheet = smart.Sheets.get_sheet(sheet.data.id)
+#         logger.info(f'[+] Sheet {sheet_name} Created')
+#         return
 
-    else:
-        logger.info(f'[+] Attaching New Data to {sheet_name}')
+#     else:
+#         logger.info(f'[+] Attaching New Data to {sheet_name}')
 
-    os.remove('/tmp/data.csv')
+#     os.remove('/tmp/data.csv')
+
+def upload_sheet(sheet):
+    logger.info(f'[+] Uploading Sheet: {sheet.title}')
+    sheet = smartsheet.Folders.create_sheet_in_folder(
+        FOLDER_ID,
+        sheet
+    )
+
+def create_sheet(sheet_name, data):
+    sheet = smartsheet.sheets.Sheet()
+    sheet.title = sheet_name
+
+    column_headers = data[0]
+    count = 0
+    for column_name in column_headers:
+        col = smartsheet.models.column.Column()
+        if count == 0:
+            col.primary = True
+        else:
+            col.primary = False
+        col.title = column_name
+        sheet.columns.append(col)
+        count += 1
 
 
 
 def lambda_handler(event, context):
     logger.info(f'[+] Lambda Invocation Starting')
     data, sheet_name = get_file_from_s3(event)
-    upload_to_smartsheet(data, sheet_name)
+    sheet = create_sheet(data, sheet_name)
+    upload_sheet(sheet)
 
     return {
         'statusCode': 200,
